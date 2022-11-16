07bf6dfecda99908d1d95e07dc06d8396a3316f0
3443ozan <108177751+3443ozan@users.noreply.github.com>
2022-11-16 10:30:32 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-class dataArray {
-    constructor(name,pass){
-        this.name = name
-        this.pass = pass
+let dataBase = [
+    {
+        name: 'vahid',
+        pass: 'valorant'
+    },
+    {
+        name:'ozan',
+        pass:'chamber'
+    },
+    {
+        name:'bind',
+        pass:'reyna'
+    },
+    {
+        name:'mother',
+        pass:'child'
     }
-}
-let dataBase = 0
+]
 const username = document.getElementById('username')
 const password = document.getElementById('password')
 const logincClick = document.getElementById('login-btn')
-const singupClick = document.getElementById('singup-btn')
 
-
-singupClick.addEventListener('click',() => {
-    dataBase = new dataArray(username.value,password.value)
-})
-
-logincClick.addEventListener('click',() => {
-    if(dataBase.name == username.value && dataBase.pass == password.value){
-        alert('xos geldin ')
+logincClick.addEventListener('click',()=>{
+    if(dataBase.find(element => element.name == username.value) && dataBase.find(element => element.pass == password.value)){
+        alert('salam')
     }
     else{
-       alert('sehv giris');
+        alert('yanlis')
     }
 })
